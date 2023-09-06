411b224f9bf182ce92e6909dfe99eb753778d6b4
Daniel Lopes de Oliveira <rm90431@estudante.fieb.edu.br>
2023-09-06 16:07:19 -0300

    Versao TiroTriplo

---
+++ FlattyBirb01/Assets/Scripts/Player.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+public class Player : MonoBehaviour
+{
+    public float velocidade = 0.0f ;
+    public float entradaHorizontal ;
+    public float entradaVertical ;
+
+    public GameObject pfLaser ;
+    
+    public float tempoDeDisparo = 0.3f ;
+    public float podeDisparar = 0.0f ;
+
+    public bool possoDarDisparoTriplo = false ;
+
+    public GameObject disparoTriplo ;
+
+    public int vidas = 3;
+
+    [SerializeField]
+    private GameObject _explosaoPlayerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start de "+this.name);
+        velocidade = 3.0f ;
+        transform.position = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Movimento();
+ 
+        
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+
+            if ( Time.time > podeDisparar ){
+               
+               if (possoDarDisparoTriplo == true ){
+
+                   Instantiate(disparoTriplo,transform.position + new Vector3(0,0,0),Quaternion.identity);
+
+               } else {   
+
+                   Instantiate(pfLaser,transform.position + new Vector3(1.8f,0,0),Quaternion.identity);
+               }
+               
+               podeDisparar = Time.time + tempoDeDisparo ;
+            
+            }
+
+ 
+        }
+       
+   }
+
+
+   private void Movimento(){
+
+       entradaHorizontal = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right*Time.deltaTime*velocidade*entradaHorizontal);
+
+        if ( transform.position.x  > 9.85f) {
+            transform.position = new Vector3(9.85f,transform.position.y,0);
+        }
+
+        if ( transform.position.x  < -9.85f  ) {
+            transform.position = new Vector3(-9.85f,transform.position.y,0);
+        
+        }
+
+        entradaVertical = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up*Time.deltaTime*velocidade*entradaVertical);
+
+        if ( transform.position.y  > 3.8f ) {
+            transform.position = new Vector3(transform.position.x,3.8f,0);
+        }
+
+        if ( transform.position.y  < -3.8f  ) {
+            transform.position = new Vector3(transform.position.x,-3.8f,0);
+        }
+
+
+
+
+   }
+
+    public IEnumerator DisparoTriploRotina()
+    {
+        yield return new WaitForSeconds(7.0f);
+        possoDarDisparoTriplo = false;
+
+    }
+
+
+    public void LigarPUDisparoTriplo()
+    {
+        possoDarDisparoTriplo = true;
+        StartCoroutine(DisparoTriploRotina());
+    }
+
+
+    public void DanoAoPlayer()
+    {
+        // vidas = vidas - 1;
+        vidas--;
+
+        if ( vidas < 1 )
+        {
+            Instantiate(_explosaoPlayerPrefab,transform.position,Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+        
+
+    }
+
+}
